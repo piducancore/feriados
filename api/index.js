0f71299d40b8db7d0c160ad59110d59f97aa474b
@piducancore <hola@piducan.dev>
2022-03-22 16:53:30 -0300

    add option to query by date

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
-import { scrapeHolidays } from "./src/scrape";
+import { scrapeHolidays } from "../src/scrape";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const holidays = await scrapeHolidays();
-  res.status(200).json(holidays);
+  const { fecha } = req.query || req.body;
+
+  if (fecha) {
+    const isHoliday = holidays.find((x) => x.fecha === fecha);
+    res.status(200).json(isHoliday ? isHoliday : "No es feriado.");
+  } else {
+    res.status(200).json(holidays);
+  }
 }
